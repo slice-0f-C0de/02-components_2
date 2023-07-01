03f81ec73c0457d83c021705f69064245054e280
slice of code <slice_of_code@mail.ru>
2023-07-01 20:11:21 +0400

    второй видос Димыча про компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Rating} from "./components/Rating/Rating";
+import {Accordion} from "./components/Accordion/Accordion";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <AppTitle/>
+        <Rating/>
+        <Accordion/>
     </div>
   );
+}
+
+function AppTitle() {
+    return <div>This is app component</div>
 }
 
 export default App;
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Star() {
+    return <div>star</div>
+}
+
+export function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
