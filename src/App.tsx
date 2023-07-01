106e85052a4911b9d92034d1d751f1e14d0a25cf
slice of code <slice_of_code@mail.ru>
2023-07-01 20:43:49 +0400

    второй видос Димыча про компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import {Rating} from "./components/Rating/Rating";
-import {Accordion} from "./components/Accordion/Accordion";
 
 function App() {
   return (
@@ -11,6 +9,43 @@ function App() {
         <Accordion/>
     </div>
   );
+}
+
+function Star() {
+    return <div>star</div>
+}
+
+export function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
+
+export function Accordion() {
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
 }
 
 function AppTitle() {
